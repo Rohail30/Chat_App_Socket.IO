1695a62dddf67cf839ea58df4badf1b08828f078
Rohail Rathore <RohailRathore10@hotmail.com>
2025-09-10 02:52:12 +0500

    Chat Schema, Routes and functionality Addded

---
+++ Backend/controllers/chatController.js
@@ -0,0 +1,75 @@
+import User from "../schemas/User";
+import Chat from "../../my-app/src/pages/Chat/Chat";
+
+const getChat = async (req, res) => {
+  const userId = req.userId;
+  const reciever = req.params.reciever;
+
+  if (!userId || !reciever) {
+    return res.status(400).json({ error: true, message: 'Sender and Reciever are required' });
+  }
+
+  try {
+    let chat = await Chat.findOne({ participants: { $all: [userId, reciever] }, })
+      .populate('messages.from', 'name');
+
+    if (!chat) {
+      chat = new Chat({ participants: [userId, reciever], messages: [], });
+      await chat.save();
+    }
+
+    return res.status(200).json(chat ? chat.messages : []);
+  } catch (error) {
+    return res.status(500).json({ error: true, message: error.message });
+  }
+};
+
+
+
+const handleSocketMessage = async (io, socket, { sender, receiver, text }) => {
+  const roomId = [sender, receiver].sort().join('_');
+
+  let chat = await Chat.findOne({ participants: { $all: [sender, receiver] }, });
+
+  if (!chat) {
+    chat = new Chat({ participants: [sender, receiver], messages: [], });
+  }
+
+  const message = { from: sender, text };
+  chat.messages.push(message);
+  await chat.save();
+
+  await chat.populate('messages.from', 'name');
+  const populatedMessage = chat.messages[chat.messages.length - 1];
+
+  io.to(roomId).emit('receiveMessage', populatedMessage);
+};
+
+
+
+const getChatUsers = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const chats = await Chat.find({ participants: userId });
+
+    const userIds = [];
+    chats.forEach((chat) => {
+      chat.participants.forEach((participant) => {
+        const participantId = participant.toString();
+        if (participantId !== userId.toString() && !userIds.includes(participantId)) {
+          userIds.push(participantId);
+        }
+      });
+    });
+
+    const users = await User.find({ _id: { $in: userIds } })
+      .populate('name email')
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { getChat, handleSocketMessage, getChatUsers };
---
+++ Backend/routes/chatRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+const { getChat, getChatUsers } = require('../controllers/chatController');
+const verifyToken = require('../middleware/verifyToken');
+
+// @desc    Get chat between two users
+router.get('/api/chat/:reciever', verifyToken, getChat);
+
+// @desc    Get all chats
+router.get('/chat/users', verifyToken, getChatUsers);
+
+export default router;
